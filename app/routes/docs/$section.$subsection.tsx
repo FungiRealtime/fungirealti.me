7862594dc777fcc6587faeca7264352fd9f26023
Gabriel Mendez <yo@gabrielmendezc.com>
2021-04-28 20:06:13 +0200

    More progress on docs

--- app/routes/docs/$section.$subsection.tsx
+++ app/routes/docs/$section.$subsection.tsx
@@ -1,8 +1,16 @@
-import { json } from "@remix-run/node";
+import { json, MetaFunction } from "@remix-run/node";
 import { LoaderFunction, useRouteData } from "@remix-run/react";
 import { getMDXComponent } from "mdx-bundler/client";
 import { useMemo } from "react";
 import { BundledMdx, getBundledMdx } from "../../github.server";
+
+export let meta: MetaFunction = ({ data }) => {
+  let { frontmatter } = data as BundledMdx;
+  return {
+    title: `${frontmatter.title} - Fungi Docs`,
+    description: frontmatter.description,
+  };
+};
 
 export let loader: LoaderFunction = async ({ params }) => {
   let bundledMdx = await getBundledMdx(
@@ -18,13 +26,24 @@ export default function DocsPage() {
 
   return (
     <div className="flex">
-      <div>
-        <h1>{frontmatter.title}</h1>
-        <p>{frontmatter.description}</p>
+      <div className="flex-1">
+        <div className="text-gray-500 flex-1 pt-8 pb-10 border-b border-gray-200">
+          <h1 className="text-gray-900 font-bold text-3xl">
+            {frontmatter.title}
+          </h1>
+          <p className="mt-2 text-lg">{frontmatter.description}</p>
+        </div>
+        <div className="prose text-gray-500 pt-8 pb-12">
+          <Component />
+        </div>
       </div>
-      <main>
-        <Component />
-      </main>
+
+      <nav className="hidden xl:block pl-12 w-64 pt-10 overflow-y-auto max-h-[84vh] sticky top-16">
+        {/* <h5 className="text-gray-900 uppercase tracking-wide font-semibold mb-3 text-sm lg:text-xs">
+          On this page
+        </h5> */}
+        {/* TODO: Get the sections headers and links */}
+      </nav>
     </div>
   );
 }
