d7d028e9b96c67c2d555633b98cea1232b17b983
Gabriel Mendez <yo@gabrielmendezc.com>
2021-04-30 20:15:38 +0200

    Update prismjs syntax highlighting

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
 
 let brandColor = "#f95858";
 
@@ -7,6 +8,47 @@ module.exports = {
   purge: ["./app/**/*.tsx", "./app/**/*.jsx", "./app/**/*.js", "./app/**/*.ts"],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+
+      black: "#000",
+      white: "#fff",
+
+      amber: colors.amber,
+      blue: colors.blue,
+      cyan: colors.cyan,
+      emerald: colors.emerald,
+      fuchsia: colors.fuchsia,
+      gray: colors.coolGray,
+      green: colors.green,
+      indigo: colors.indigo,
+      "light-blue": colors.lightBlue,
+      lime: colors.lime,
+      orange: {
+        ...colors.orange,
+        1000: "#4a2008",
+      },
+      pink: {
+        ...colors.pink,
+        1000: "#460d25",
+      },
+      purple: colors.purple,
+      red: colors.red,
+      rose: colors.rose,
+      teal: colors.teal,
+      violet: colors.violet,
+      yellow: colors.yellow,
+
+      code: {
+        punctuation: "#A1E8FF",
+        tag: "#D58FFF",
+        "attr-name": "#4BD0FB",
+        "attr-value": "#A2F679",
+        string: "#A2F679",
+        highlight: "rgba(134, 239, 172, 0.25)",
+      },
+    },
     extend: {
       colors: {
         brand: brandColor,
