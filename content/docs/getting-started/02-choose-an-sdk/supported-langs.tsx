9a91d268c313b4dbeddf95885e5643c08035aa6c
Gabriel Mendez <yo@gabrielmendezc.com>
2021-05-04 17:07:50 +0200

    Update content

--- content/docs/getting-started/02-choose-an-sdk/supported-langs.tsx
+++ content/docs/getting-started/02-choose-an-sdk/supported-langs.tsx
@@ -1,6 +1,6 @@
 export function SupportedLangs() {
   return (
-    <div className="grid grid-cols-2 sm:grid-cols-3 gap-4 text-center">
+    <div className="flex space-x-4 space-y-4 flex-wrap">
       <img src="/images/js.svg" className="w-16 h-16" />
       <img src="/images/nodejs.svg" className="w-16 h-16" />
     </div>
