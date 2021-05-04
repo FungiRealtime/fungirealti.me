3e3f5585c707492ed2da9ec664d5012ac23384f1
Gabriel Mendez <yo@gabrielmendezc.com>
2021-05-04 17:08:37 +0200

    Update content

--- content/docs/getting-started/02-choose-an-sdk/supported-langs.tsx
+++ content/docs/getting-started/02-choose-an-sdk/supported-langs.tsx
@@ -1,6 +1,6 @@
 export function SupportedLangs() {
   return (
-    <div className="flex space-x-4 space-y-4 flex-wrap">
+    <div className="flex flex-wrap gap-4">
       <img src="/images/js.svg" className="w-16 h-16" />
       <img src="/images/nodejs.svg" className="w-16 h-16" />
     </div>
