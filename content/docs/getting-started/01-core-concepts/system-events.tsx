7594c99924b4edd0d0c34ef6839a64c3d15cb8b7
Gabriel Mendez <yo@gabrielmendezc.com>
2021-05-05 19:22:23 +0200

    Update content

---
+++ content/docs/getting-started/01-core-concepts/system-events.tsx
@@ -0,0 +1,112 @@
+import React from "react";
+
+interface SystemEvent {
+  name: string;
+  payload: any;
+  description: string;
+}
+
+let systemEvents: SystemEvent[] = [
+  {
+    name: "fungi:connection_established",
+    payload: {
+      socket_id: "string", // The unique identifier for the socket
+      activity_timeout: "number", // The interval to send ping events to the server
+    },
+    description:
+      "Event sent when the client successfully establishes a connection with the server.",
+  },
+  {
+    name: "fungi:subscription_succeeded",
+    payload: {
+      channel: "string", // The name of the channel
+    },
+    description:
+      "Event sent when the client successfully subscribes to a channel.",
+  },
+  {
+    name: "fungi:subscription_error",
+    payload: {
+      channel: "string", // The name of the channel
+      reason: "string", // The reason the subscription wasn't successful
+    },
+    description: "Event sent when the client fails to subscribe to a channel.",
+  },
+  {
+    name: "fungi:unsubscription_succeeded",
+    payload: {
+      channel: "string", // The name of the channel
+    },
+    description:
+      "Event sent when the client successfully unsubscribes from a channel.",
+  },
+  {
+    name: "fungi:error",
+    payload: {
+      message: "string", // A textual description of the error
+      code: "number", // Fungi re-uses HTTP error codes, 401 for Unauthorized, etc.
+    },
+    description:
+      "Event sent by Fungi when an action attempted by a client fails (like authorization for a private channel).",
+  },
+  {
+    name: "fungi:pong",
+    payload: null,
+    description:
+      "Event sent by Fungi in response to pings sent by a client SDK to keep the connection alive.",
+  },
+];
+
+export function SystemEvents() {
+  return (
+    <div className="flex flex-col">
+      <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+        <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+          <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+            <table className="min-w-full divide-y divide-gray-200">
+              <thead className="bg-gray-50">
+                <tr>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Event name
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Payload
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Description
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                {systemEvents.map((event) => (
+                  <tr key={event.name} className="bg-white">
+                    <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                      {event.name}
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                      <pre>
+                        <code>{JSON.stringify(event.payload, null, 2)}</code>
+                      </pre>
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                      {event.description}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
