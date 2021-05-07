7782c4b727e4d7c07765cb4b754639f385af7d08
Gabriel Mendez <yo@gabrielmendezc.com>
2021-05-07 21:22:44 +0200

    Update content

---
+++ content/docs/javascript/02-channels/channel-api.tsx
@@ -0,0 +1,76 @@
+import React from "react";
+
+let api = [
+  {
+    name: "bind",
+    signature: "bind<TData>;(event: string, handler: EventBindHandler<TData>",
+    description: "Binds to an event on the channel.",
+    args: [
+      { name: "event", description: "The name of the event to bind to." },
+      {
+        name: "handler",
+        description: "The function to run when the event is triggered.",
+      },
+      { name: "options?", description: "Additional options for the binding." },
+    ],
+  },
+];
+
+export function ChannelAPI() {
+  return (
+    <div className="flex flex-col">
+      <div className="overflow-x-auto">
+        <div className="py-2 align-middle inline-block min-w-full">
+          <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+            <table className="min-w-full divide-y divide-gray-200">
+              <thead className="bg-gray-50">
+                <tr>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Method or attribute
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Description
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Arguments
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                {api.map((something) => (
+                  <tr key={something.name} className="bg-white">
+                    <td className="flex flex-col space-y-3 px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                      <span>{something.name}</span>
+                      <code>{something.signature}</code>
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                      {something.description}
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                      <ul>
+                        {something.args.map((arg) => (
+                          <li key={arg.name}>
+                            {arg.name}: {arg.description}
+                          </li>
+                        ))}
+                      </ul>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
