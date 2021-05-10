5eebd11399594cdc202cb6febdfb718d72443d49
Gabriel Mendez <yo@gabrielmendezc.com>
2021-05-10 18:23:41 +0200

    Add dockerr hub to icons and upddate redirect status code for docs

--- app/routes/docs/index.tsx
+++ app/routes/docs/index.tsx
@@ -1,7 +1,9 @@
 import { LoaderFunction, redirect } from "remix";
 
 export let loader: LoaderFunction = () => {
-  return redirect("/docs/01-getting-started/01-overview");
+  return redirect("/docs/01-getting-started/01-overview", {
+    status: 303,
+  });
 };
 
 export default function GettingStarted() {
